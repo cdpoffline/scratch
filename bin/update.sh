b516654bc7054dd00a7f2f068949a45f9b12f9a3
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-31 14:19:23 +0000

    only downloading when newer

--- bin/update.sh
+++ bin/update.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+cd "`dirname \"$0\"`"
+
 cd ../web
 
-rm -r downloads
-mkdir downloads
+mkdir -p downloads
 cd downloads
 
 
-mkdir "Scratch 2"
+mkdir -p "Scratch 2"
 cd "Scratch 2"
 # from https://scratch.mit.edu/scratch2download/
 # 
@@ -19,73 +20,73 @@ cd "Scratch 2"
 #   openssl s_client -showcerts -servername scratch.mit.edu -connect scratch.mit.edu:443 </dev/null
 #
 # TODO: openssl shows that everything is fine but
-#       wget still does not accept the certificates
+#       wget -c -N still does not accept the certificates
 
 
-mkdir Linux
+mkdir -p Linux
 cd Linux
 echo "Scratch for Linux"
 
-wget http://airdownload.adobe.com/air/lin/download/2.6/AdobeAIRInstaller.bin
-wget --no-check-certificate https://scratch.mit.edu/scratchr2/static/sa/Scratch-442.air
+wget -c -N http://airdownload.adobe.com/air/lin/download/2.6/AdobeAIRInstaller.bin
+wget -c -N --no-check-certificate https://scratch.mit.edu/scratchr2/static/sa/Scratch-442.air
 
 cd ..
-mkdir Windows
+mkdir -p Windows
 cd Windows
 echo "Scratch for Windows"
 
-wget http://airdownload.adobe.com/air/win/download/20.0/AdobeAIRInstaller.exe
-wget --no-check-certificate https://scratch.mit.edu/scratchr2/static/sa/Scratch-442.exe
+wget -c -N http://airdownload.adobe.com/air/win/download/20.0/AdobeAIRInstaller.exe
+wget -c -N --no-check-certificate https://scratch.mit.edu/scratchr2/static/sa/Scratch-442.exe
 
 cd ..
-mkdir MacOS
+mkdir -p MacOS
 cd MacOS
 echo "Scratch for Mac OS 10.5 or older"
 
-wget http://airdownload.adobe.com/air/mac/download/2.6/AdobeAIR.zip
-wget --no-check-certificate https://scratch.mit.edu/scratchr2/static/sa/Scratch-442.air
+wget -c -N http://airdownload.adobe.com/air/mac/download/2.6/AdobeAIR.zip
+wget -c -N --no-check-certificate https://scratch.mit.edu/scratchr2/static/sa/Scratch-442.air
 
 cd ..
-mkdir "MacOS X"
+mkdir -p "MacOS X"
 cd "MacOS X"
 echo "Scratch for MacOS X"
 
-wget http://airdownload.adobe.com/air/mac/download/20.0/AdobeAIR.dmg
-wget --no-check-certificate https://scratch.mit.edu/scratchr2/static/sa/Scratch-442.dmg
+wget -c -N http://airdownload.adobe.com/air/mac/download/20.0/AdobeAIR.dmg
+wget -c -N --no-check-certificate https://scratch.mit.edu/scratchr2/static/sa/Scratch-442.dmg
 
 cd ..
-mkdir "Support Materials"
+mkdir -p "Support Materials"
 cd "Support Materials"
 echo "Support Materials"
 
-wget --no-check-certificate https://scratch.mit.edu/scratchr2/static/sa/Scratch2StarterProjects.zip
-wget --no-check-certificate https://cdn.scratch.mit.edu/scratchr2/static/__29cebbddad1a2303315dedb62fd76793__/pdfs/help/Getting-Started-Guide-Scratch2.pdf
-wget --no-check-certificate https://cdn.scratch.mit.edu/scratchr2/static/__29cebbddad1a2303315dedb62fd76793__/pdfs/help/Scratch2Cards.pdf
+wget -c -N --no-check-certificate https://scratch.mit.edu/scratchr2/static/sa/Scratch2StarterProjects.zip
+wget -c -N --no-check-certificate https://cdn.scratch.mit.edu/scratchr2/static/__29cebbddad1a2303315dedb62fd76793__/pdfs/help/Getting-Started-Guide-Scratch2.pdf
+wget -c -N --no-check-certificate https://cdn.scratch.mit.edu/scratchr2/static/__29cebbddad1a2303315dedb62fd76793__/pdfs/help/Scratch2Cards.pdf
 
 cd ../.. # downloads
 
-mkdir "Scratch 1"
+mkdir -p "Scratch 1"
 cd "Scratch 1"
 # from --no-check-certificate https://scratch.mit.edu/scratch_1.4/
 
-mkdir Linux
+mkdir -p Linux
 cd Linux
 echo "Scratch for Linux"
 
-wget http://ubuntu.media.mit.edu/ubuntu//pool/universe/s/scratch/scratch_1.4.0.6~dfsg1-5~ubuntu12.04.1_all.deb
+wget -c -N http://ubuntu.media.mit.edu/ubuntu//pool/universe/s/scratch/scratch_1.4.0.6~dfsg1-5~ubuntu12.04.1_all.deb
 
 cd ..
-mkdir Windows
+mkdir -p Windows
 cd Windows
 echo "Scratch for Windows"
 
-wget --no-check-certificate https://download.scratch.mit.edu/ScratchInstaller1.4.exe
+wget -c -N --no-check-certificate https://download.scratch.mit.edu/ScratchInstaller1.4.exe
 
 cd ..
-mkdir "MacOS X"
+mkdir -p "MacOS X"
 cd "MacOS X"
 echo "Scratch for MacOS X"
 
-wget --no-check-certificate https://download.scratch.mit.edu/MacScratch1.4.dmg
+wget -c -N --no-check-certificate https://download.scratch.mit.edu/MacScratch1.4.dmg
 
 cd ../.. # downloads
